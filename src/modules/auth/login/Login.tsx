c6fdb36f7170eff54213fc27bc6dc98e50ff2eed
Michał Latra <michal.latra@gmail.com>
2022-12-27 16:37:48 +0100

    [POW-18] Login page improvements

--- src/modules/auth/login/Login.tsx
+++ src/modules/auth/login/Login.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import "../../../styles/auth.css";
 import LoginForm from "./components/LoginForm";
 import { LoginData } from "../../../models/api/LoginData";
+import { Link, useNavigate } from "react-router-dom";
+import { PathEnum } from "../../../enums/PathEnum";
 
 const Login = () => {
+  const navigate = useNavigate();
   const handleSubmitLogin = (formData: LoginData) => {
     console.log(formData);
+    navigate("/" + PathEnum.DRIVE);
   };
 
   return (
@@ -13,6 +17,9 @@ const Login = () => {
       <div className="app__auth__card">
         <h2 className="app__auth__card__header">Logowanie</h2>
         <LoginForm onSubmit={handleSubmitLogin} isLoading={false} />
+        <div className="app__auth__card__links">
+          <Link to={"/" + PathEnum.REGISTER}>Zarejestruj się</Link>
+        </div>
       </div>
     </div>
   );
