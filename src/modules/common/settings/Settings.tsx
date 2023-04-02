04641935c5b4bb6c25686ddf9993229a22479122
cinis <bartlomiejdabrowski2000@gmail.com>
2023-04-02 19:42:19 +0200

    basic containers, not fully styled yet

--- src/modules/common/settings/Settings.tsx
+++ src/modules/common/settings/Settings.tsx
@@ -1,8 +1,38 @@
 import React, { Component } from 'react'
-
+import "../../../styles/settings.css"
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 const Settings = () =>{
     return (
-        <div></div>
+        <div
+            className='app__settings__main' 
+        >
+            <div className='app__set__header'>
+                <div style={{
+                    padding:"auto"
+                }}>
+                    <ArrowBackIcon style={{fontSize:"2rem", paddingRight:"0.75rem"}}/>   
+                </div>
+                Ustawienia
+                
+            </div>
+            <div className='app__settings__account'>
+                <div>
+                    Ustawienia konta
+                </div>
+                <div>
+                    Zmiana hasla
+                </div>
+            </div>
+            <div>
+                Ustawienia dysku
+            </div>
+            <div>
+                Ustawienia Kalendarza
+            </div>
+            <div>
+                Ustawienia Notatek
+            </div>
+        </div>
     )
 }
 export default Settings;
